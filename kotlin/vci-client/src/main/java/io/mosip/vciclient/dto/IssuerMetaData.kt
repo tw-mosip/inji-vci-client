f4289000348bebdb7674f741c16a2eaf6f410e57
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-09-06 07:23:33 +0530

    [INJIMOB-1078] format IssuerMetadata file
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- kotlin/vci-client/src/main/java/io/mosip/vciclient/dto/IssuerMetaData.kt
+++ kotlin/vci-client/src/main/java/io/mosip/vciclient/dto/IssuerMetaData.kt
@@ -6,10 +6,12 @@ class IssuerMetaData(
     val credentialAudience: String,
     val credentialEndpoint: String,
     val downloadTimeoutInMilliSeconds: Int,
-    val credentialType: Array<String>?=null,
+    val credentialType: Array<String>? = null,
     val credentialFormat: CredentialFormat,
-    val docType: String?=null,
-    val claims: Map<String,Any>?=null
-)
+    val docType: String? = null,
+    val claims: Map<String, Any>? = null,
+
+
+    )
 
 
