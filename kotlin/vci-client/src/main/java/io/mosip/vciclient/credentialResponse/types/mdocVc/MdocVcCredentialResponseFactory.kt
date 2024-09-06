cfefee66f87a8b46a77a3d813e11cbfd95b42209
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-09-06 07:18:16 +0530

    [INJIMOB-1078] refactor redundant code in mdoc parsing logic
    Other changes:
    - Move mdoc parsing logic to Mso mdoc credential response factory
    
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- kotlin/vci-client/src/main/java/io/mosip/vciclient/credentialResponse/types/mdocVc/MdocVcCredentialResponseFactory.kt
+++ kotlin/vci-client/src/main/java/io/mosip/vciclient/credentialResponse/types/mdocVc/MdocVcCredentialResponseFactory.kt
@@ -1,91 +1,125 @@
 package io.mosip.vciclient.credentialResponse.types.mdocVc
 
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
-import io.mosip.vciclient.common.FileWriter
+import co.nstant.`in`.cbor.CborDecoder
+import co.nstant.`in`.cbor.model.DataItem
+import co.nstant.`in`.cbor.model.MajorType
+import co.nstant.`in`.cbor.model.Special
+import co.nstant.`in`.cbor.model.UnicodeString
+import co.nstant.`in`.cbor.model.UnsignedInteger
+import io.mosip.vciclient.common.Encoder
 import io.mosip.vciclient.common.JsonUtils
-import io.mosip.vciclient.common.Util
 import io.mosip.vciclient.credentialResponse.CredentialResponse
 import io.mosip.vciclient.credentialResponse.CredentialResponseFactory
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import java.util.function.Consumer
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonArray
+import kotlinx.serialization.json.buildJsonObject
+import java.io.ByteArrayInputStream
+import kotlin.collections.forEach
+import co.nstant.`in`.cbor.model.Array as CborArray
+import co.nstant.`in`.cbor.model.ByteString as CborByteString
+import co.nstant.`in`.cbor.model.Map as CborMap
+import co.nstant.`in`.cbor.model.UnicodeString as CborUnicodeString
 
 class MdocVcCredentialResponseFactory : CredentialResponseFactory {
-    private val logTag = Util.getLogTag(javaClass.simpleName)
     override fun constructResponse(response: String): CredentialResponse {
-        val mdocJsonString = MdocParser.decodeAndParseMDocData(response).toString()
-        val mdocInJson = Json.decodeFromString<JsonObject>(mdocJsonString)
-//        largeLog(logTag, "mdoc json ${mdocInJson.jsonObject.get("issuerSigned")?.jsonObject?.get("nameSpaces")}")
-        largeLog(logTag, "mdoc json ${mdocInJson}")
-        /*mdocInJson.entries.forEach(Consumer {
-            val key = it.key
-            println("$key is json ${it.value is JsonObject}")
-            parse(it.value, 1)
-        })
-
-        val data =
-            mdocInJson["issuerSigned"]?.jsonObject?.get("nameSpaces")?.jsonObject?.get("org.iso.18013.5.1")?.jsonArray
-        if (data != null) {
-            for (dataElement in data) {
-                val jsonObject = dataElement.jsonObject
-                println("${jsonObject["elementIdentifier"]}: ${jsonObject["elementValue"]}, random - ${jsonObject["random"]}")
-            }
-        }*/
-        return JsonUtils.deserialize(
-            mdocJsonString,
-            MdocCredential::class.java
-        )!!
+        val deserializedResponse = JsonUtils.deserialize(response, HashMap::class.java)
+        val mdocJson: JsonObject = decodeAndParseMDocData(
+            deserializedResponse?.get("credential")
+                .toString()
+        )
+        val mdocCredential = MdocCredential(credential = mdocJson)
+        return mdocCredential
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun parse(value: JsonElement, space: Int) {
-        when (value) {
-            is JsonObject -> {
-                value.jsonObject.entries.forEach(Consumer {
-                    val key = it.key
-                    println("${printSpace(space)}$key")
-                    parse(it.value, space + 1)
-                })
+    private fun decodeAndParseMDocData(base64EncodedUrl: String): JsonObject {
+        val decodedData: ByteArray =
+            Encoder().decodeFromBase64UrlFormatEncoded(base64EncodedUrl)
+
+        val mDocVcJsonObject = buildJsonObject {
+            val cbors: MutableList<DataItem> =
+                CborDecoder(ByteArrayInputStream(decodedData)).decode()
+            cbors.forEach { cbor ->
+                run {
+                    (cbor as CborMap).keys.forEach { key ->
+                        val identifier: String = key.toString()
+                        run {
+                            put(identifier, parse(cbor[identifier]))
+                        }
+                    }
+                }
+            }
+        }
+        return mDocVcJsonObject
+    }
+
+    private fun parseArray(cbor: CborArray): JsonArray {
+        val jsonArray = buildJsonArray {
+            cbor.dataItems.forEach { arrayElement ->
+                add(parse(arrayElement))
+            }
+        }
+        return jsonArray
+    }
+
+    private fun parseMap(cbor: CborMap): JsonObject {
+        val jsonObject = buildJsonObject {
+            cbor.keys.forEach { key ->
+                val element = cbor[key]
+                run {
+                    put(key.toString(), parse(element))
+                }
+            }
+        }
+        return jsonObject
+    }
+
+    private fun parse(cbor: DataItem): JsonElement {
+        when (cbor.majorType) {
+            MajorType.ARRAY -> {
+                return parseArray(cbor as CborArray)
             }
 
-            is JsonArray -> {
-                value.jsonArray.forEach(Consumer {
-                    println("-> Array")
-                    parse(it, space + 1)
-                })
+            MajorType.MAP -> {
+                return parseMap(cbor as CborMap)
             }
 
-            else -> {
-                println("-> string")
+            MajorType.SPECIAL -> {
+                return JsonPrimitive((cbor as Special).toString())
             }
+
+            MajorType.BYTE_STRING -> {
+                try {
+                    val decoded =
+                        CborDecoder(ByteArrayInputStream((cbor as CborByteString).bytes)).decode()
+
+                    return parse(decoded[0])
+                } catch (e: Exception) {
+                    return JsonPrimitive(String((cbor as CborByteString).bytes))
+                } catch (e: OutOfMemoryError) {
+                    return JsonPrimitive(String((cbor as CborByteString).bytes))
+
+                }
+            }
+
+            MajorType.UNSIGNED_INTEGER -> {
+                return JsonPrimitive((cbor as UnsignedInteger).toString())
+            }
+
+            MajorType.UNICODE_STRING -> {
+                return JsonPrimitive((cbor as UnicodeString).toString())
+            }
+
+            else -> return JsonPrimitive(null)
         }
     }
 
-    private fun printSpace(space: Int): String {
-        var result = ""
-        var i = space
-        while (i > 0) {
-            result += " ";
-            i -= 1
-        }
-        return result
-    }
 
-
-}
-
-fun largeLog(tag: String?, content: String) {
-    if (content.length > 4000) {
-        Log.d(tag, content.substring(0, 4000))
-        largeLog(tag, content.substring(4000))
-    } else {
-        Log.d(tag, content)
+    operator fun DataItem.get(name: String): DataItem {
+        check(this.majorType == MajorType.MAP)
+        this as CborMap
+        return this.get(CborUnicodeString(name))
     }
 }
