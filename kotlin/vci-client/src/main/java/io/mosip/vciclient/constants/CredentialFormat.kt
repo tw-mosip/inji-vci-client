0abb26c64b8e86ef2f480e46733d8354fa6cd2e9
Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
2025-07-28 10:51:41 +0530

    [INJIMOB-3365] add dc+sd-jwt credential format support
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- kotlin/vci-client/src/main/java/io/mosip/vciclient/constants/CredentialFormat.kt
+++ kotlin/vci-client/src/main/java/io/mosip/vciclient/constants/CredentialFormat.kt
@@ -3,5 +3,6 @@ package io.mosip.vciclient.constants
 enum class CredentialFormat(val value: String) {
     LDP_VC("ldp_vc"),
     MSO_MDOC("mso_mdoc"),
-    SD_JWT("vc+sd-jwt"),
+    VC_SD_JWT("vc+sd-jwt"),
+    DC_SD_JWT("dc+sd-jwt"),
 }
--- kotlin/vci-client/src/main/java/io/mosip/vciclient/issuerMetadata/IssuerMetadataService.kt
+++ kotlin/vci-client/src/main/java/io/mosip/vciclient/issuerMetadata/IssuerMetadataService.kt
@@ -116,16 +116,18 @@ class IssuerMetadataService {
                 )
             }
 
-            CredentialFormat.SD_JWT.value -> {
+            CredentialFormat.VC_SD_JWT.value, CredentialFormat.DC_SD_JWT.value -> {
                 val vct = credentialType["vct"] as? String
                     ?: throw IssuerMetadataFetchException("Missing vct for SD-JWT")
 
                 val claims = credentialType["claims"] as? Map<String, Any>
+                val resolvedFormat = CredentialFormat.values().firstOrNull { it.value == format }
+                    ?: throw IssuerMetadataFetchException("Unrecognized credential format: $format")
 
                 IssuerMetadata(
                     credentialIssuer = credentialIssuer,
                     credentialEndpoint = credentialEndpoint,
-                    credentialFormat = CredentialFormat.SD_JWT,
+                    credentialFormat = resolvedFormat,
                     vct = vct,
                     claims = claims,
                     scope = scope,
