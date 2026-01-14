d77a86f4e09ed8a464b69c303580cca3e9fc2f6e
abhip2565 <paul.apaul.abhishek.AP@gmail.com>
2026-01-14 14:27:06 +0530

    [INJIMOB-3247] update test dependencies
    Signed-off-by: abhip2565 <paul.apaul.abhishek.AP@gmail.com>

--- kotlin/vci-client/build.gradle.kts
+++ kotlin/vci-client/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     implementation("com.squareup.okio:okio:3.6.0")
     implementation("io.inji:inji-openid4vp-aar:0.7.0-RC1")
+    testImplementation(kotlin("test"))
 }
 
 tasks {
