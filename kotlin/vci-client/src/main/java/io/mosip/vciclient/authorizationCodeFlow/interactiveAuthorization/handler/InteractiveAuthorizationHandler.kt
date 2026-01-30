abcd6a9a526e99a078edee662e95fafbcadf4d51
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-01-30 15:10:43 +0530

    [INJIMOB-3720] refactor: handle rename of poperty name
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- kotlin/vci-client/src/main/java/io/mosip/vciclient/authorizationCodeFlow/interactiveAuthorization/handler/InteractiveAuthorizationHandler.kt
+++ kotlin/vci-client/src/main/java/io/mosip/vciclient/authorizationCodeFlow/interactiveAuthorization/handler/InteractiveAuthorizationHandler.kt
@@ -166,7 +166,7 @@ class InteractiveAuthorizationHandler {
         val authorizationService = PresentationDuringIssuanceAuthorizationMethodService(
             selectCredentialsForPresentation = presentationMethod.selectCredentialsForPresentation,
             signVerifiablePresentation = presentationMethod.signVerifiablePresentation,
-            signatureSuite = presentationMethod.ldpVpSignatureSuite,
+            ldpVpSignatureSuite = presentationMethod.ldpVpSignatureSuite,
             traceabilityId = traceabilityId
         )
 
