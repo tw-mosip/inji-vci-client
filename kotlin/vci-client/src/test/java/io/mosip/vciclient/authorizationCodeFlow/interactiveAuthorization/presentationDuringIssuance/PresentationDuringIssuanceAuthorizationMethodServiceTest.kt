fc141ebaf65ca8cacc56dc5ac343e1cff12fdc91
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-07-03 19:38:51 +0530

    refactor: update Inji ovp dependency to 1.0.0-alpha.1-SNAPSHOT
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- kotlin/vci-client/src/test/java/io/mosip/vciclient/authorizationCodeFlow/interactiveAuthorization/presentationDuringIssuance/PresentationDuringIssuanceAuthorizationMethodServiceTest.kt
+++ kotlin/vci-client/src/test/java/io/mosip/vciclient/authorizationCodeFlow/interactiveAuthorization/presentationDuringIssuance/PresentationDuringIssuanceAuthorizationMethodServiceTest.kt
@@ -159,7 +159,7 @@ class PresentationDuringIssuanceAuthorizationMethodServiceTest {
     @Test
     fun `should successfully authorize and return success response`() = runTest {
         coEvery { mockOvp.constructUnsignedVPToken(any()) } returns listOf(
-            UnsignedVPToken(FormatType.LDP_VC, "k1","ES256", "unsigned".toByteArray())
+            UnsignedVPToken(id = "id1",FormatType.LDP_VC, "k1","ES256", "unsigned".toByteArray())
         )
 
         coEvery { mockOvp.constructVPResponse(any()) } returns mapOf("vp_token" to "signed")
@@ -178,6 +178,7 @@ class PresentationDuringIssuanceAuthorizationMethodServiceTest {
             signVerifiablePresentation = {
                 listOf(
                     VPTokenSigningResult(
+                        "id1",
                         signedData = "signed".toByteArray(),
                     )
                 )
